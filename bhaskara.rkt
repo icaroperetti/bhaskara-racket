264e0f580349f7b2dd5318abca5b55bc12b3fca7
Icaro Peretti <icaroperetti50@gmail.com>
2020-10-27 16:02:22 -0300

    Faltando o calculo dos vertices

--- bhaskara.rkt
+++ bhaskara.rkt
@@ -5,18 +5,18 @@
   (-(* b b ) (* 4 a c))
 )
 
-(define (find-x1 a b c)
+(define (findX2 a b c)
   (/ (+(- b) (sqrt (delta a b c))) (* 2 a))
 )
 
-(define (find-x2 a b c)
+(define (findX1 a b c)
   (/ (-(- b) (sqrt (delta a b c))) (* 2 a))
 )
 
 (define (bhaskara a b c)
-  (~a "delta=" (delta a b c) " x1=" (find-x1 a b c)" x2="(find-x2 a b c))
+  (~a "delta=" (delta a b c) " x1=" (findX1 a b c)" x2="(findX2 a b c))
 )
 
-(define (say-results a b c)
+(define (show-results a b c)
   (write(bhaskara a b c))
  )
